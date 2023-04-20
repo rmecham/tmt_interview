1b4a18edbe978110ccaa3858124ece9725dc83bd
Rob Mecham <stemmetje@gmail.com>
2023-04-20 12:00:58 -0600

    Basic filtering for orders.

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.db.models import QuerySet
 from rest_framework import generics
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
+
 # Create your views here.
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    
+
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        start_date = self.request.query_params.get('start_date')
+        if start_date:
+            queryset = queryset.filter(start_date__gte=start_date)
+        embargo_date = self.request.query_params.get('embargo_date')
+        if embargo_date:
+            queryset = queryset.filter(embargo_date__lte=embargo_date)
+        return queryset
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
